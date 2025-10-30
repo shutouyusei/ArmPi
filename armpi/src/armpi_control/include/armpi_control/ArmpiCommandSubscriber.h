0578717533ddafacf56f91052074cb6e6bb25487
shutouyusei <syutou.yusei.061@s.kyushu-u.ac.jp>
2025-10-30 18:08:49 +0900

    [ref]some refactoring

--- armpi/src/armpi_control/include/armpi_control/ArmpiCommandSubscriber.h
+++ armpi/src/armpi_control/include/armpi_control/ArmpiCommandSubscriber.h
@@ -14,8 +14,6 @@ private:
 
   ros::NodeHandle nh_;
   ros::Subscriber sub_cmd_;
-  int robot_state_ = 0;
 
   void cmdCallback(const armpi_operation_msgs::RobotCommand::ConstPtr &msg);
-  std::string velocity2String(const std::vector<double>& velocity);
 };
--- armpi/src/armpi_control/src/ArmpiCommandSubscriber.cpp
+++ armpi/src/armpi_control/src/ArmpiCommandSubscriber.cpp
@@ -14,16 +14,3 @@ void ArmpiCommandSubscriber::cmdCallback(const armpi_operation_msgs::RobotComman
   ROS_INFO("armpi_command received");
   drive_function_(msg->base_velocity);
 }
-
-std::string ArmpiCommandSubscriber::velocity2String(const std::vector<double>& velocity) {
-  std::string joint_velocities_str = "";
-  for (size_t i = 0; i < velocity.size(); ++i)
-  {
-      joint_velocities_str += std::to_string(velocity[i]);
-      if (i < velocity.size() - 1)
-      {
-          joint_velocities_str += ", ";
-      }
-  }
-  return joint_velocities_str;
-}
