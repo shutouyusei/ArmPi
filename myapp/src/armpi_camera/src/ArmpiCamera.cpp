bf9b468f983a4b1969f96cfd9b7efe7974c3710a
shutouyusei <syutou.yusei.061@s.kyushu-u.ac.jp>
2025-11-07 12:11:41 +0900

    [add]camera subscriber

--- myapp/src/armpi_camera/src/ArmpiCamera.cpp
+++ myapp/src/armpi_camera/src/ArmpiCamera.cpp
@@ -3,27 +3,15 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 
-// カメラ画像を受け取るコールバック関数
 void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
   try {
-    // 1. ROS ImageメッセージをOpenCVのcv::Matに変換
-    // cv_bridge::toCvShare() を使用して、メッセージデータを共有し、変換オーバーヘッドを削減
     cv_bridge::CvImageConstPtr cv_ptr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8);
-
-    // 2. OpenCVの画像データ (cv::Mat) を取得
     cv::Mat image_bgr = cv_ptr->image;
 
-    // 3. 画像処理ロジックの開始
     if (image_bgr.empty()) {
       ROS_ERROR("Received empty image!");
       return;
     }
-
-    //TODO:カメラの映像を保存できるか
-    // ここに画像処理ロジック（例: cv2.cvtColor, cv2.inRange, 顔認識ネット処理など）を記述します
-    // 4. (デバッグ用) 画像の表示
-    // cv::imshow("Camera Feed", image_bgr);
-    // cv::waitKey(1); // 描画処理のために必要
 
     ROS_INFO("Image received: %d x %d (Type: %s)", 
              image_bgr.cols, image_bgr.rows, 
@@ -36,10 +24,10 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
 }
 
 int main(int argc, char** argv) {
+  ROS_INFO("Setup Subscriber for image");
   // 1. ROSノードの初期化
   ros::init(argc, argv, "cpp_image_subscriber");
   ros::NodeHandle nh;
-  cv::startWindowThread();
   // 2. image_transport の初期化
   image_transport::ImageTransport it(nh);
 
