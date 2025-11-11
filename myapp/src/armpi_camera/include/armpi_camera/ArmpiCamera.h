4d4dbaa02d6ee5fc1337728fcd9b673f7cbbdfd0
shutouyusei <syutou.yusei.061@s.kyushu-u.ac.jp>
2025-11-11 09:49:20 +0900

    create armpi_camera class

---
+++ myapp/src/armpi_camera/include/armpi_camera/ArmpiCamera.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <vector>
+
+
+class ArmpiCamera {
+public:
+  ArmpiCamera(ros::NodeHandle& nh);
+  ~ArmpiCamera();
+
+  void start();
+  void finish();
+
+  void getCollectedImages(std::vector<cv_bridge::CvImageConstPtr>& images);
+
+private:
+  void imageCallback(const sensor_msgs::ImageConstPtr& msg);
+private:
+  std::vector<cv_bridge::CvImageConstPtr> collected_images_;
+  ros::NodeHandle nh_;
+  image_transport::ImageTransport it_;
+  image_transport::Subscriber sub_;
+};
--- myapp/src/armpi_camera/src/ArmpiCamera.cpp
+++ myapp/src/armpi_camera/src/ArmpiCamera.cpp
@@ -1,44 +1,34 @@
-#include <ros/ros.h>
-#include <image_transport/image_transport.h>
-#include <opencv2/highgui/highgui.hpp>
-#include <cv_bridge/cv_bridge.h>
+#include <armpi_camera/ArmpiCamera.h>
 
-void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
+ArmpiCamera::ArmpiCamera(ros::NodeHandle& nh):nh_(nh),it_(nh){
+  ROS_INFO("Setup Subscriber for image");
+}
+
+ArmpiCamera::~ArmpiCamera(){}
+
+void ArmpiCamera::imageCallback(const sensor_msgs::ImageConstPtr& msg) {
   try {
     cv_bridge::CvImageConstPtr cv_ptr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8);
-    cv::Mat image_bgr = cv_ptr->image;
-
-    if (image_bgr.empty()) {
-      ROS_ERROR("Received empty image!");
-      return;
-    }
-
-    ROS_INFO("Image received: %d x %d (Type: %s)", 
-             image_bgr.cols, image_bgr.rows, 
-             cv_ptr->encoding.c_str());
-
+    // save images
+    collected_images_.push_back(cv_ptr);
   } catch (cv_bridge::Exception& e) {
     ROS_ERROR("cv_bridge exception: %s", e.what());
     return;
   }
 }
 
-int main(int argc, char** argv) {
-  ROS_INFO("Setup Subscriber for image");
-  // 1. ROSノードの初期化
-  ros::init(argc, argv, "cpp_image_subscriber");
-  ros::NodeHandle nh;
-  // 2. image_transport の初期化
-  image_transport::ImageTransport it(nh);
+void ArmpiCamera::start() {
+  ROS_INFO("Starting ArmpiCamera...");
+  sub_ = it_.subscribe("/usb_cam/image_raw", 1, &ArmpiCamera::imageCallback,this);
+}
 
-  // 3. /usb_cam/image_raw トピックの購読
-  // image_transport::Subscriber を使用して、Imageメッセージを購読する
-  image_transport::Subscriber sub = it.subscribe("/usb_cam/image_raw", 1, imageCallback);
+void ArmpiCamera::finish() {
+  ROS_INFO("Shutting down ArmpiCamera...");
+  sub_.shutdown();
+  // clear buffer
+  collected_images_.clear();
+}
 
-  ROS_INFO("Listening for images on /usb_cam/image_raw...");
-
-  // 4. メッセージ処理ループ
-  ros::spin(); 
-
-  return 0;
+void ArmpiCamera::getCollectedImages(std::vector<cv_bridge::CvImageConstPtr>& images) {
+  images = collected_images_;
 }
