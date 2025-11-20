db948817ea370ca1c5ae1b74f6543ab92f53f21a
shutouyusei <syutou.yusei.061@s.kyushu-u.ac.jp>
2025-11-20 11:31:38 +0900

    fix

--- scripts/rsync_armpi.sh
+++
@@ -1,1 +0,0 @@
-rsync -avz --delete --exclude '.git' --exclude '__pycache__' --progress $PWD/ros/armpi/ pi@raspberrypi.local:~/docker/src/armpi
--- scripts/rsync_share.sh
+++
@@ -1,1 +0,0 @@
-rsync -avz --delete --exclude '.git' --exclude '__pycache__' --progress $PWD/ros/share/ pi@raspberrypi.local:~/docker/src/share
